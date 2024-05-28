e4f1a53787e16d611940d3acd8ed8d82f374e011
dev-song <dvlprsong@gmail.com>
2024-05-28 14:54:03 +0900

    #noissue Modify Vite and router config for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/dog-land'>
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/map' element={<Map />} />
